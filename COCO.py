5320fa90017923b61e71b8f206786c226c164452
Viktor Vodnev <v.vodnev73@gmail.com>
2022-01-06 20:11:08 +0100

    Script now parse only one file

--- COCO.py
+++ COCO.py
@@ -13,21 +13,23 @@ import argparse
 import logging
 from zipfile import ZipFile
 
+URL = "http://images.cocodataset.org/annotations/annotations_trainval2017.zip"
+JSON_NAME = "person_keypoints_val2017.json"
+
 def parse_arguments():
-    logging.info("Start parsing arguments ")
+    logging.info("Start parsing arguments")
     try:
         parser = argparse.ArgumentParser(description='Arguments get parsed via --commands')
         parser.add_argument("-u", type=str, default=None, required=True, help="Enter the link to the file")
         parser.add_argument("-p", type=str, default=None, required=True, help="Enter the path for uploading files")
         args = parser.parse_args()
     except:
-        logging.exception("Error reading arguments ")
+        logging.exception("Error reading arguments")
     finally:
         logging.debug("Argument reading completed successfully")
 
     return args
 
-#Download zip archive from the link
 def get_archive(URL):
     logging.info("Downloading the archive")
     try:
@@ -38,75 +40,61 @@ def get_archive(URL):
     finally:
         logging.debug("Successful download of the archive")
 
-#Extract files from zip archive and transfer their location
-def file_path():
+# check if file exist an return path
+def unzip():
     logging.info("The extraction of files from the zip archive starts ")
     try:
-        jsons_path = []
         zip_path = os.path.dirname(__file__) + '/archive.zip'
         with ZipFile(zip_path) as z:
             z.extractall(str(os.path.dirname(__file__)))
             for filename in z.namelist():
-                if(filename[-1] != '/'):
-                    jsons_path.append(str(os.path.dirname(__file__)) + '/' + str(filename))
+                if(JSON_NAME in filename):
+                    json_path = str(os.path.dirname(__file__)) + '/' + str(filename)
+                    return json_path
     except:
-        logging.exception("Error extracting files from zip archive ")
+        logging.exception("Error extracting files from zip archive")
     finally:
-        logging.debug("File extraction successfully")
-    return jsons_path
+        logging.debug("File extracted successfully")
 
-#Extracting jason file to variable 
-def open_load():
-    json_f = []
-    for path in file_path():
-        f = open(path)
-        json_f.append(json.load(f))
-        f.close()
 
+def open_load(json_path):
+    f = open(json_path)
+    json_f = json.load(f)
+    f.close()
     return json_f
 
-#Parsing a jason file into a dataframe 
 
-def df_convert(jfile):
-    
+def df_convert(json_file):
     columns  = ["label", "image_name", "image_width", "image_height", "x_min", "y_min", "x_max", "y_max", "image_url"]
     data = []
     df = pd.DataFrame(data, columns=columns)
-    images = jfile['images']
-    annotations = jfile['annotations']
-    categories = jfile['categories']
-
+    images = json_file['images']
+    annotations = json_file['annotations']
+    categories = json_file['categories']
     for i in range(len(images)):
         for j in range(len(annotations)):
             if(images[i]['id'] == annotations[j]['image_id']):
-                for k in range(len(categories)):
-                    if(annotations[j]['category_id'] == categories[k]['id']):
-                        df = df.append({'label':categories[k]['name'], 'image_name':images[i]['file_name'], 
-                        'image_width':images[i]['width'], 'image_height':images[i]['height'], 
-                        'x_min':annotations[j]['bbox'][0], 'y_min':annotations[j]['bbox'][0] , 
-                        'x_max': round(annotations[j]['bbox'][2] - annotations[j]['bbox'][0], 3), 
-                        'y_max': round(annotations[j]['bbox'][3] - annotations[j]['bbox'][1], 3), 
-                        'image_url':images[i]['coco_url']}, ignore_index=True)
+                df = df.append({'label':categories[0]['name'], 'image_name':images[i]['file_name'], 
+                'image_width':images[i]['width'], 'image_height':images[i]['height'], 
+                'x_min':annotations[j]['bbox'][0], 'y_min':annotations[j]['bbox'][0] , 
+                # x_max = x_min + width of bounding box
+                'x_max': round(annotations[j]['bbox'][2] + annotations[j]['bbox'][0], 3),
+                # y_max = y_min + height of bounding box
+                'y_max': round(annotations[j]['bbox'][3] + annotations[j]['bbox'][1], 3), 
+                'image_url':images[i]['coco_url']}, ignore_index=True)
     return df
 
-#Names for csv files 
-def name_parse():
-    names = []
-    for path in file_path():
-        names.append((path.split('/')[-1]).split('.')[0])
-    return names
 
-def df_to_csv(df, path):
+def df_to_csv(df, csv_path):
     logging.info("Converting date frame to csv file")
     try:
-        names = name_parse()
-        for j in range(len(names)):
-            df.to_csv(path + names[j] + '.csv', index=False)
+        df.to_csv(csv_path + JSON_NAME.replace('.json', '.csv'), index=False)
         logging.debug("Successful writing to file ")
     except:
-        logging.exception("Errors during file creation")
+        logging.exception("Errors during file convertion")
     finally:
         logging.debug("The file is completely filled with data")
+
 
 #Removing temporary files
 def rm_tmp():
@@ -125,17 +113,13 @@ def rm_tmp():
 
 def main():
     logging.basicConfig(level=logging.DEBUG, filename='coco_info.log',
-     format='%(asctime)s %(levelname)s:%(message)s')
-    args = parse_arguments()
-    URL = args.u
-    UP_PATH = args.p
+        format='%(asctime)s %(levelname)s:%(message)s')
+    #args = parse_arguments()
     get_archive(URL)
-    jsons = open_load()
-    for i in range(len(jsons)):
-        df = df_convert(jsons[i])
-        df_to_csv(df, UP_PATH)
+    csv_path = "/home/vv173/"
+    df = df_convert(open_load(unzip()))
+    df_to_csv(df.head(5), csv_path)
     rm_tmp()
-
 
 if __name__ == '__main__':
     main()
